cd5e5637b4ee25646f0b20c02538aef172a8e3ff
Pratik <pratikkesharwani448@gmail.com>
2024-04-26 13:08:33 -0400

    Added carousel functionality
    completed the javaScript part of carousle

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,12 @@ const preloader = document.querySelector(".preloader");
 const preloaderTextOne = document.querySelector(".pre-span-one");
 const preloaderTextTwo = document.querySelector(".pre-span-two");
 
+const carousel = document.querySelector(".carousel");
+
+let isDragging = false;
+let startX = "";
+let startscrollLeft = "";
+
 window.addEventListener("load", function () {
   setTimeout(() => {
     preloaderTextOne.classList.add("preloader--deactive");
@@ -14,5 +20,29 @@ window.addEventListener("load", function () {
 
   setTimeout(() => {
     preloader.classList.add("preloader--hide");
+    document.documentElement.style.setProperty("--body-scroll-hide", "block");
   }, 2000);
 });
+
+// Carousel
+
+const startDragging = function (e) {
+  isDragging = true;
+  startX = e.pageX;
+  startscrollLeft = carousel.scrollLeft;
+  carousel.classList.add("carousel--selected");
+};
+
+const dragging = function (e) {
+  if (!isDragging) return;
+  carousel.scrollLeft = startscrollLeft - (e.pageX - startX);
+};
+
+const stopDragging = function () {
+  isDragging = false;
+  carousel.classList.remove("carousel--selected");
+};
+
+carousel.addEventListener("mousedown", startDragging);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", stopDragging);
