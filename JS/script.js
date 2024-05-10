6cb842a5573ae36e0c3301fbaffc96df5a88b7dc
Pratik <pratikkesharwani448@gmail.com>
2024-05-10 12:43:00 -0400

    added lenis
    lenis is a library. It enhance our scroll experience.

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,25 @@
 "use strict";
 
+// LENIS
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+// --------------------------------
+
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
   grabCursor: true,
+  speed: 600,
 
   breakpoints: {
     1199: {
@@ -38,3 +54,45 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+// const rev = function () {
+//   const slidesIndex = [3, 4, 5, 6];
+
+//   const slideCard = slidesIndex.map((i) => {
+//     return swiper.slides[i];
+//   });
+
+//   slideCard.forEach((item) => {
+//     item
+//       .querySelector(".fifth--title")
+//       .setAttribute("style", `transform = translate3d(0,-100px,0)`);
+//     item
+//       .querySelector(".para--fourth")
+//       .setAttribute("style", `transform = translate3d(0,-100px,0)`);
+//   });
+// };
+
+// const initi = function () {
+//   let as = swiper.activeIndex;
+//   const slidesIndex = [as, as + 1, as + 2];
+
+//   const slideCard = slidesIndex.map((i) => {
+//     return swiper.slides[i];
+//   });
+
+//   slideCard.forEach((item) => {
+//     item
+//       .querySelector(".fifth--title")
+//       .setAttribute("style", `transform = translate3d(0,0,0)`);
+//     item
+//       .querySelector(".para--fourth")
+//       .setAttribute("style", `transform = translate3d(0,0,0)`);
+//   });
+
+//   rev();
+// };
+
+// swiper.on("slideChangeTransitionEnd", initi);
+
+// initi();
+// rev();
