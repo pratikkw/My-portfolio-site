7d5fa014b576c0f87271bbd064a2450adfc35bd0
Pratik <pratikkesharwani448@gmail.com>
2024-05-11 13:20:08 -0400

    Added some hover
    Added some hover animation for carousel links.

--- JS/script.js
+++ JS/script.js
@@ -61,28 +61,70 @@ const carouselCards = document.querySelectorAll(".carousel__card");
 carouselCards.forEach((card) => {
   let title = card.querySelector(".fifth--title");
   let para = card.querySelector(".para--fourth");
+  let links = card.querySelectorAll(".carousel__link");
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: card,
+      start: "top center",
+      markers: false,
+    },
+
+    ease: "sine.out",
+  });
 
   gsap.set(card, {
     x: 1000,
     opacity: 0.3,
   });
 
-  gsap.to(card, {
-    scrollTrigger: {
-      trigger: card,
-      start: "top center",
-    },
+  gsap.set(title, {
+    y: 100,
+    opacity: 0,
+  });
+
+  gsap.set(para, {
+    y: 150,
+    opacity: 0,
+  });
+
+  gsap.set(links, {
+    x: 150,
+    opacity: 0,
+  });
+
+  tl.to(card, {
     x: 0,
     opacity: 1,
-    ease: "power2.out",
     duration: 2,
+    ease: "power3.out",
   });
+
+  tl.to(
+    [title, para],
+    {
+      y: 0,
+      opacity: 1,
+      duration: 0.5,
+    },
+    "-=0.5"
+  );
+
+  tl.to(
+    links,
+    {
+      x: 0,
+      opacity: 1,
+    },
+    "-=0.25"
+  );
 });
 // --------------------------------
 
 const preloader = document.querySelector(".preloader");
 const preloaderTextOne = document.querySelector(".preloader__text-one");
 const preloaderTextTwo = document.querySelector(".preloader__text-two");
+const swiperBox = document.querySelector(".swiper-wrapper");
 
 const showSite = function () {
   preloader.classList.add("opacity-zero");
@@ -98,4 +140,21 @@ window.addEventListener("load", function () {
     preloaderTextTwo.classList.add("opacity-zero");
     showSite();
   }, 1000);
+});
+
+swiperBox.addEventListener("mouseover", function (e) {
+  const targetEle = e.target;
+
+  if (!targetEle.classList.contains("carousel__icon")) return;
+  const targetEleTwo = targetEle.closest(".carousel__link");
+  const linkBox = targetEle.closest(".carousel__links");
+  const child = linkBox.children;
+
+  [...child].forEach((item) => {
+    if (item != targetEleTwo) item.style.opacity = "0.15";
+  });
+
+  linkBox.addEventListener("mouseout", function (e) {
+    [...child].forEach((item) => (item.style.opacity = "1"));
+  });
 });
