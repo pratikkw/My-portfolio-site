09c924430cd709a9f3202a93fe310e706f1b4893
Pratik <pratikkesharwani448@gmail.com>
2024-05-18 12:50:23 -0400

    Getting the carousel card dynamically.
    Using the asynchronous feature in js to get the carousel card dynamically.

--- JS/script.js
+++ JS/script.js
@@ -121,15 +121,64 @@ carouselCards.forEach((card) => {
 });
 // --------------------------------
 
-// SPLIT TYPE
-// --------------------------------
-
 const preloader = document.querySelector(".preloader");
 const preloaderTextOne = document.querySelector(".preloader__text-one");
 const preloaderTextTwo = document.querySelector(".preloader__text-two");
 const swiperBox = document.querySelector(".swiper-wrapper");
 const bulbBtn = document.querySelector(".bulb");
 const toggleAudio = new Audio("../audio/on-off-sound.mp3");
+
+// GET Carousel
+const getCarousel = async function () {
+  const result = await fetch("JSON/project.json");
+  const r = await result.json();
+
+  swiperBox.innerHTML = r
+    .map((item) => {
+      return `<li class="carousel__card swiper-slide" data-projectID="${item.projectID}">
+    <div class="carousel__img">
+      <img src=${item.projectImg} alt="" />
+    </div>
+    <div class="carousel__txt">
+      <div class="carousel__sub-txt grid">
+        <div class="txt__box-three">
+          <h5 class="fifth--title">${item.projectName}</h5>
+          <p class="para--fourth">${item.desc}</p>
+        </div>
+
+        <div class="carousel__links flex">
+          <a
+            href="${item.siteLink}"
+            target="_blank"
+            class="carousel__link"
+            ><ion-icon
+              class="carousel__icon"
+              name="link-outline"
+            ></ion-icon
+          ></a>
+          <a
+            href="${item.gitLink}"
+            target="_blank"
+            class="carousel__link"
+            ><ion-icon
+              class="carousel__icon"
+              name="logo-github"
+            ></ion-icon
+          ></a>
+          <a href="#" class="carousel__link"
+            ><ion-icon
+              class="carousel__icon"
+              name="ellipsis-horizontal-outline"
+            ></ion-icon
+          ></a>
+        </div>
+      </div>
+    </div>
+  </li>`;
+    })
+    .join("");
+};
+// --------------------------------
 
 // Preloader
 const showSite = function () {
@@ -141,6 +190,7 @@ const showSite = function () {
 };
 
 window.addEventListener("load", function () {
+  getCarousel();
   setTimeout(() => {
     preloaderTextOne.classList.add("opacity-zero");
     preloaderTextTwo.classList.add("opacity-zero");
@@ -240,6 +290,7 @@ tl.to(textTwo.words, {
   opacity: 1,
   stagger: 0.25,
   ease: "sine.out",
+  duration: 1,
 });
 
 // --------------------------------
