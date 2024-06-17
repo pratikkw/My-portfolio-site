2029536d43dd614314b3da952cbf97e17f19ee72
Pratik <pratikkesharwani448@gmail.com>
2024-06-17 10:34:46 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -114,7 +114,7 @@ const getCarousel = async function () {
               name="logo-github"
             ></ion-icon
           ></a>
-          <a href="../HTML/detail.html?id=${item.projectId}" class="carousel__link"
+          <a href="HTML/detail.html?id=${item.projectId}" class="carousel__link"
             ><ion-icon
               class="carousel__icon"
               name="ellipsis-horizontal-outline"
